92a930c922e4924ada2833bc5d0b31d4e4977625
Brian Jameson <bxj231@gmail.com>
2014-11-01 21:59:51 -0700

    add user create and unicorn

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: full_name(user), class: "gravatar")
+  end
+
+  def full_name(user)
+    first_and_last = [user.first_name, user.last_name].join(" ")
+  end
 end
