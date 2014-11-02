8c6cbacb4cac4317b2b19817c142f45f0a2a03fc
Brian Jameson <bxj231@gmail.com>
2014-11-01 22:31:27 -0700

    add session support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Check Me Out!"
       redirect_to @user
     else
